92992c22ef9c9c768f9ddae28746c73cb05cf77f
Sergey Sinitsyn <sin_s_n@mail.ru>
2016-12-18 01:53:47 +0300

    realized InMemoryUserRepositoryImpl

---
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -0,0 +1,68 @@
+package ru.javawebinar.topjava.repository.mock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.repository.UserRepository;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+/**
+ * GKislin
+ * 15.06.2015.
+ */
+@Repository
+public class InMemoryUserRepositoryImpl implements UserRepository {
+
+    private static final Logger LOG = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+
+    private final Map<Integer, User> repository = new ConcurrentHashMap<>();
+
+    private AtomicInteger counter = new AtomicInteger(0);
+
+    @Override
+    public boolean delete(int id) {
+        LOG.info("delete " + id);
+        return null == repository.remove(id);
+    }
+
+    @Override
+    public User save(User user) {
+        LOG.info("save " + user);
+        Objects.requireNonNull(user);
+        if (user.isNew()) {
+            user.setId(counter.incrementAndGet());
+        }
+        repository.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public User get(int id) {
+        LOG.info("get " + id);
+        return repository.get(id);
+    }
+
+    @Override
+    public List<User> getAll() {
+        LOG.info("getAll");
+        if (!repository.isEmpty()) {
+            return repository.values().stream()
+                    .sorted(Comparator.comparing(User::getName))
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        LOG.info("getByEmail " + email);
+        return this.getAll().stream()
+                .filter(u -> u.getEmail().equals(email))
+                .findFirst().orElse(null);
+    }
+}
