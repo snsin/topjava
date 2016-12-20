3f68fd70cbcfbd092ecf0881213ea3377aef0edc
Sergey Sinitsyn <sin_s_n@mail.ru>
2016-12-21 00:00:22 +0300

    realized MealService interface and MealServiceImpl class

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,8 +1,28 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  * GKislin
  * 15.06.2015.
  */
+
 public interface MealService {
+
+    Meal save(Meal meal, int userId);
+
+    void delete(int id, int userId) throws NotFoundException;
+
+    Meal get(int id, int userId) throws NotFoundException;
+
+    List<Meal> getAll(int userId);
+
+    void update(Meal meal, int userId);
+
+    public List<Meal> getBetweenDates(int userId, LocalDate startDate, LocalDate endDate);
+
 }
--- src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -1,13 +1,55 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
 
 /**
  * GKislin
  * 06.03.2015.
  */
+
+@Service
 public class MealServiceImpl implements MealService {
 
+    @Autowired
     private MealRepository repository;
 
+    @Override
+    public Meal save(Meal meal, int userId) {
+        return repository.save(meal, userId);
+    }
+
+    @Override
+    public void delete(int id, int userId) throws NotFoundException {
+        checkNotFoundWithId(repository.delete(id, userId), id);
+    }
+
+    @Override
+    public Meal get(int id, int userId) throws NotFoundException {
+        return checkNotFoundWithId(repository.get(id, userId), id);
+    }
+
+    @Override
+    public List<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+    @Override
+    public void update(Meal meal, int userId) {
+        checkNotFoundWithId(repository.save(meal, userId) != null,
+                Math.toIntExact(meal.getId()));
+    }
+
+    @Override
+    public List<Meal> getBetweenDates(int userId, LocalDate startDate, LocalDate endDate) {
+        return repository.getBetweenDates(userId, startDate, endDate);
+    }
 }
