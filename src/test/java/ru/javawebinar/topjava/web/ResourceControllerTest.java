35d4f07fd0ab9c7c7e12bbbe3eee8fb21e2d48ea
Sergey Sinitsyn <sin_s_n@mail.ru>
2017-01-30 00:24:00 +0300

    created ResourceControllerTest.java for style.css

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,22 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.Test;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by smac on 29.01.17.
+ */
+public class ResourceControllerTest extends AbstractControllerTest {
+
+    @Test
+    public void testCss() throws Exception {
+        mockMvc.perform(get("/resources/css/style.css"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/css"));
+    }
+}
