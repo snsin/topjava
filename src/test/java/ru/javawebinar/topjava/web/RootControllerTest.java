a6d705ee0e4c56b7324a89f5bcf6ef360ff7f633
Sergey Sinitsyn <sin_s_n@mail.ru>
2017-01-29 22:40:48 +0300

    added meals.jsp test

--- src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/RootControllerTest.java
@@ -1,12 +1,19 @@
 package ru.javawebinar.topjava.web;
 
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.USER;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
 
 /**
@@ -14,6 +21,24 @@ import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
  * 10.04.2015.
  */
 public class RootControllerTest extends AbstractControllerTest {
+    @Test
+    public void testMeals() throws Exception {
+        mockMvc.perform(get("/meals"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("meals"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/meals.jsp"))
+                .andExpect(model().attribute("meals", hasSize(6)))
+                .andExpect(model().attribute("meals", hasItems(
+                        hasProperty("id", is(MEAL6.getId())),
+                        hasProperty("id", is(MEAL5.getId())),
+                        hasProperty("id", is(MEAL4.getId())),
+                        hasProperty("id", is(MEAL3.getId())),
+                        hasProperty("id", is(MEAL2.getId())),
+                        hasProperty("id", is(MEAL1.getId()))
+                )));
+
+    }
 
     @Test
     public void testUsers() throws Exception {
