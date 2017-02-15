312066b2ace68314467ce17096ba4a49ad468cd0
Sergey Sinitsyn <sin_s_n@mail.ru>
2017-02-15 23:02:01 +0300

    changed request parameters for getBetween

--- src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -89,7 +89,7 @@ public class MealRestControllerTest extends AbstractControllerTest {
 
     @Test
     public void testGetBetween() throws Exception {
-        mockMvc.perform(get(REST_URL + "between?startDateTime=2015-05-30T07:00&endDateTime=2015-05-31T11:00:00")
+        mockMvc.perform(get(REST_URL + "between?startDateTime=2015-05-30 07:00&endDateTime=2015-05-31 11:00")
                 .with(userHttpBasic(USER)))
                 .andExpect(status().isOk())
                 .andDo(print())
